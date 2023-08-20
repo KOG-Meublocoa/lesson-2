9a90600d96c52eb7ed6203b3da753b4994609edc
KOG-Meublocoa <duongdangkhoa071@gmail.com>
2023-08-20 11:27:23 +0700

    buổi 2

---
+++ js.js
@@ -0,0 +1,71 @@
+// let myArray =[4 , 5 , 7 , 8 , 9 , 1],
+// console.log(myArray);
+
+// for(let i =0; i < myArray.length;i++) {
+//     console.log("Gia tri cu phan tu thu" i ,"la" , myArray[i])
+// }
+
+// myArray[2]=6;
+// console.log(myArray)
+
+// myArray.splice(2,1,7,8);
+// console.log(myArray);
+
+// console.log(myArray,length); //độ dài mảng
+
+// myArray.push(7)
+// console.log(myArray);
+
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(0)
+// console.log(myArray); // thêm phần tử đầu tiên
+
+// myArray.shift()
+// console.log(myArray); // xóa phần tử đầu tiên
+
+
+// let dog ={
+//     name:Siba
+//     age:3
+// }
+
+
+// dog.name = 'Meublocoa'
+// console.log(dog);
+
+// dog.action='Walk'
+// console.log(dog);
+
+// delete(dog.action)
+// console.log(dog);
+
+
+// let cat= new Object({
+//     name:'Tom',
+//     age:3,
+//     enemy:{
+//         name :'Alaska',
+//         age:5
+//     }
+// }
+// );
+
+// let dog1 =["Alaska",4]
+// let dog2 =["Corgi",5]
+
+// let dogs = [dog1,dog2]
+
+// console.log(dogs);
+
+// console.log(dogs[1][0] );
+
+let gioTraiCay = [
+    ['xoai',2],
+    ['dua hau',3],
+]
+
+for(let i =0 ;i<gioTraiCay.length;i++){
+    console.log("Gio trai cay:" + gioTraiCay[i][0]+ gioTraiCay[i][1]);
+}
